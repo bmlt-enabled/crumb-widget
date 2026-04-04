b8505d3e9cb4f9867f13627c2bca89ad6041ecd2
Patrick Joyce <pjaudiomv@gmail.com>
2026-04-03 23:42:40 -0400

    bottom

--- src/components/MapView.svelte
+++ src/components/MapView.svelte
@@ -201,7 +201,7 @@
   <div bind:this={mapEl} class="h-full min-h-96 w-full"></div>
 
   {#if showSearchArea && geoActive && onsearcharea}
-    <div class="pointer-events-none absolute top-3 right-0 left-0 z-[1000] flex justify-center">
+    <div class="pointer-events-none absolute bottom-8 right-0 left-0 z-[1000] flex justify-center">
       <button onclick={handleSearchArea} class="bmlt-map-search-btn pointer-events-auto rounded-full border px-4 py-2 text-sm font-medium shadow-lg">
         {$t.searchThisArea}
       </button>
