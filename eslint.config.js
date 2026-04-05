13f845acd442226135fe71cca86779804f4b7e00
Patrick Joyce <pjaudiomv@gmail.com>
2026-04-05 11:40:53 -0400

    entry

--- eslint.config.js
+++ eslint.config.js
@@ -45,7 +45,19 @@ export default [
         files: ['**/*.ts', '**/*.svelte', '**/*.svelte.ts'],
         rules: {
             '@typescript-eslint/no-explicit-any': 'off',
-            'no-undef': 'off'
+            'no-undef': 'off',
+            '@typescript-eslint/no-unused-vars': [
+              'error',
+              {
+                'args': 'all',
+                'argsIgnorePattern': '^_',
+                'caughtErrors': 'all',
+                'caughtErrorsIgnorePattern': '^_',
+                'destructuredArrayIgnorePattern': '^_',
+                'varsIgnorePattern': '^_',
+                'ignoreRestSiblings': true
+              }
+            ]
         }
     }
 ];
